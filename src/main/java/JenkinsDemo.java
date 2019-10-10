7fe2b239c64bdd5d77032c5f61942ba45e3daa8f
Tariq <tari0048@stud.kea.dk>
2019-10-10 13:34:18 +0200

    Added another change to the file

--- src/main/java/JenkinsDemo.java
+++ src/main/java/JenkinsDemo.java
@@ -1,5 +1,8 @@
 public class JenkinsDemo {
     public static void main(String[] args) {
         System.out.println("Hello Jarl");
+
+        System.out.println("Another test to see whether Jenkins can automatically fetch update with use of Webhook");
     }
+
 }
